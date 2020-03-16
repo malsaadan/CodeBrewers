c498b6042b2e951af74e76dfa24f2eaa4ade0226
NanZ-hash <afnanxmt@gmail.com>
2020-03-17 00:51:06 +0300

    linking the router with the main server

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ app.use(indexRoutes);
 app.use(exampleRoutes);
 app.use(userRoutes);
 app.use(menuItemRoutes);
+app.use(orderRoutes);
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
